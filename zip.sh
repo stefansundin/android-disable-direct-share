876494030a669dcfd167aa64f253a2f8bf45d828
Stefan Sundin <git@stefansundin.com>
2018-05-19 16:33:32 -0700

    rm before zipping

--- zip.sh
+++ zip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ex
+rm -f disable-direct-share-v1.zip disable-direct-share-v1-uninstall.zip
 zip -r disable-direct-share-v1.zip META-INF disable-direct-share.xml
 (cd uninstall && zip -r ../disable-direct-share-v1-uninstall.zip META-INF)
